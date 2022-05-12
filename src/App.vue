e614f874dc4a0963f258956d6139b4f681024002
DESKTOP-KSNJ8Q2\Zohreh <zfarmahini@gmail.com>
2022-05-12 13:22:16 +0430

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <!-- <nav>
+    <router-link  to="/">خانه</router-link> |
+    <router-link to="/about">درباره من</router-link>
+  </nav> -->
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <router-link class="nav-item nav-link active" to="/">خانه</router-link> 
+      <router-link class="nav-item nav-link active" to="/about">درباره من</router-link>
+      
+    </div>
+  </div>
+</nav>
   <router-view/>
+  <footer class="font-small fixed-bottom"></footer>
 </template>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main role="main" class="container">
+
+          <div v-for="item in  messageList" :key="item.id"  v-bind:class="[item.id == 1 ? 'alert alert-secondary bg-light d-flex justify-content-between' : 'alert alert-primary text-left']"   >
+          {{item.message}}</div>
+        
+            
+
+ 
+  
+    <div class="input-group   fixed-bottom" >
+    <input  type="text" class="form-control" v-model="mess" placeholder="لطفا تایپ نمایید" v-on:keyup.enter="sendMessage()"/>
+ 
+<div class="input-group-prepend">
+   <button v-on:click="sendMessage()" class="btn btn-primary ">ارسال</button>
   </div>
+</div>
+
+   <br>
+    <span v-show="visibale">ارتباط با موفقیت انجام شد</span>
+    <br>
+     
+          <span v-text="result"></span>
+     
+          
+
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      connection: null,
+      messageList:[],
+      show:null,
+      visibale:false,
+      result:null
+    }
+  },
+  methods: {
+      sendMessage: function() {  
+      this.connection.send(this.mess)
+      this.messageList.push({id:2,message:this.mess})
+    },
+     connect : function() {
+     this.connection = new WebSocket("wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self")
+     this.connection.onmessage =(message)=> {
+     console.log(message.data)
+     this.messageList.push({id:1,message:message.data})
+    
+    }
+
+    this.connection.onopen = function(event) {    
+      console.log(event)
+      console.log ("ارتباط با موفقیت انجام شد"+event.data)
+      this.show ="ارتباط با موفقیت انجام شد"
+      this.visibale = true
+     
+    }
+    this.connection.onerror = function(err) {
+      this.show = 'خطا در سوکت ', err.message, 'سوکت بسته شده است'
+      this.connection.close()
+     }
+    this.connection.onclose = function(err) {
+        console.log('سوکت بسته شده است . ارتباط مجدد'+err.message)
+         this.show ='سوکت بسته شده است . ارتباط مجدد'+err.message
+       setTimeout(function() {
+       this.connect()
+        }, 1000)
+     }
   }
+  },
+  created: function() {
+      this.connect()
+  },
+  
 }
 </script>
